6d0ca0ba1f0d17639a38dacea7ab23f2632e59f1
badass1901 <shashwatsagar19@gmail.com>
2024-02-28 01:03:05 +0530

    some style fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,9 @@ const Home = () => {
             <h6 className="text-lg">
               <em>Are you hungry?</em>
             </h6>
-            <h1 className="text-3xl md:text-6xl font-bold">Don't wait!</h1>
+            <h1 className="text-3xl md:text-6xl font-bold hero-text">
+              Don't wait!
+            </h1>
             <button className="px-6 py-2 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600">
               Order Now
             </button>
